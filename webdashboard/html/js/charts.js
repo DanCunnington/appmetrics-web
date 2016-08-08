53807e409c2970830077861c5606d9c7401ab474
Daniel Cunnington <dancunnington@uk.ibm.com>
2016-08-08 15:42:26 +0100

    express graph

--- webdashboard/html/js/charts.js
+++ webdashboard/html/js/charts.js
@@ -137,19 +137,18 @@ function initialiseSocketIO() {
 
   //express parsing
   socket.on('express', function (expressdata){
-    console.log("got express data ");
     express = JSON.parse(expressdata);  //parses the data into a JSON array
+
     if(Expressdata.getNumberOfRows() > 0){
       var oldDate = Expressdata.getValue(0,0).getTime() / 1000; // convert to seconds
       if(((express['timestamp'] / 1000) - oldDate) > 120){
         Expressdata.removeRow(0);            
       }
     }
-    Expressdata.addRow([new Date(express['timestamp']),(express.response.duration), 
-                        'Request: ' +(express.request.method)+'\n'+
-                        'URL: '+(express.request.url)+'\n'+
-                        'Response: ' +(express.response.status)+'\n'+
-                        'Data: ' +(express.process.data)]);
+    Expressdata.addRow([new Date(express['time']),(express.duration), 
+                        'Request: ' +(express.method)+'\n'+
+                        'URL: '+(express.url)+'\n'+
+                        'Response: ' +(express.statusCode)]);
     Expresschart.draw(Expressdata, Expressoptions);
   });
 
